c176a002b27b3d82a791d916721fa71b1598570e
“Will9Costa” <wildembergdcosta@gmail.com>
2024-03-12 10:49:17 -0300

    Value 1%

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let startButton = document.querySelector(".startButton");
 
 let progress;
 let progressValue = 0;
-let progressEndValue = 25;
+let progressEndValue = 1;
 let totalAmount = 3000; // Valor total em reais
 let speed = 100;
 
